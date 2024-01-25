65244c9e61a10cb36b6153a5eb281ce306200d7f
AceXiamo <xiamo@qwq.link>
2024-01-25 17:58:28 +0800

    Add GsapTest component

---
+++ components/GsapTest.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div
+      class="h-[50px] w-[50px] bg-emerald-500 rounded-md"
+      v-gsap="{ duration: 1, x: 100 }"
+    ></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
