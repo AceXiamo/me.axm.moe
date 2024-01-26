6236c13921b780b3278b3f2caff92c3d73cb0f28
AceXiamo <xiamo@qwq.link>
2024-01-26 18:01:10 +0800

    Add GSAP directive and types

--- plugins/gsap.ts
+++ plugins/gsap.ts
@@ -1,15 +1,46 @@
 import { gsap } from "gsap";
 
+type GsapDirectiveValue = { method: "from" | "to"; config: gsap.TweenVars };
+type GsapMethod = "from" | "to";
+type GsapAttr =
+  | "initial"
+  | "enter"
+  | "leave"
+  | "hovered"
+  | "tapped"
+  | "focused";
+type GsapProp = {
+  [key in GsapAttr]?: gsap.TweenVars;
+};
+
+// declare module "@vue/runtime-core" {
+//   export interface ComponentCustomProperties {
+//     $gsap?: GsapDirectiveValue;
+//   }
+// }
+
+declare module "@vue/runtime-dom" {
+  interface HTMLAttributes {
+    initial?: gsap.TweenVars & { duration?: number };
+    enter?: gsap.TweenVars;
+    leave?: gsap.TweenVars;
+    hovered?: gsap.TweenVars;
+    tapped?: gsap.TweenVars;
+    focused?: gsap.TweenVars;
+  }
+}
+
 declare module "vue" {
   interface ComponentCustomProperties {
-    vGsap: (option: { method: string; config: gsap.TweenVars }) => void;
+    vGsap: (option:GsapDirectiveValue) => void;
   }
 }
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.directive("gsap", {
-    mounted(el, binding) {
-      (gsap as any)[binding.value.method](el, binding.value.config);
+    mounted(el, binding, vnode) {
+      const { method, config } = binding.value as GsapDirectiveValue;
+      gsap[method](el, config);
     },
     getSSRProps(binding) {
       return {};
