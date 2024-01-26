7e2f9f454669ec21921ab5a705e302755ed9ee15
AceXiamo <xiamo@qwq.link>
2024-01-26 18:01:13 +0800

    Refactor the 'say' component layout and add a 'show' variable

--- pages/say.vue
+++ pages/say.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <!-- <span class="text-white">123123</span> -->
+  <div class="p-[100px]">
+    <div
+      class="h-[100px] w-[100px] bg-gray-200/20 rounded-full"
+    ></div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const show = ref(true);
+</script>
