bd1557b6657ec636621ef44631c8709a8d80b6a7
AceXiamo <xm17906193@163.com>
2024-01-26 23:04:42 +0800

    Remove unnecessary code in say.vue

--- pages/say.vue
+++ pages/say.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="p-[100px]">
-    <div
-      class="h-[100px] w-[100px] bg-gray-200/20 rounded-full"
-    ></div>
-  </div>
+  <div class="p-[100px]"></div>
 </template>
 
 <script lang="ts" setup>
-const show = ref(true);
+const show = ref(true)
 </script>
