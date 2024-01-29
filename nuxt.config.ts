9129fe40939ab9fbacc7a6cbb9254460135ae5ce
AceXiamo <xiamo@qwq.link>
2024-01-29 17:09:05 +0800

    chore: colorMode config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@unocss/nuxt", "nuxt-icon"],
-  css: ["~/assets/css/style.css"],
-  plugins: ["~/plugins/gsap.ts", "~/plugins/avatar.client.ts"],
-});
+  modules: ['@unocss/nuxt', 'nuxt-icon', '@nuxtjs/color-mode'],
+  css: ['~/assets/css/style.css'],
+  plugins: ['~/plugins/gsap.ts', '~/plugins/avatar.client.ts'],
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '',
+    storageKey: 'nuxt-color-mode',
+  },
+})
