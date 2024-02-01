11c5a910ce6f9b1a2ee09945a23d3f161803cce2
AceXiamo <xiamo@qwq.link>
2024-02-01 17:34:59 +0800

    Add avatar plugin for Vue.js

---
+++ plugins/avatar.client.tsx
@@ -0,0 +1,105 @@
+import { gsap } from 'gsap'
+import { render } from 'vue'
+import Avatar from '~/assets/images/avatar.jpg'
+
+let homeAvatarContainer: HTMLElement | null = null
+let headerAvatarContainer: HTMLElement | null = null
+let avatarFromHeader: HTMLElement | null = null
+let avatarFromHome: HTMLElement | null = null
+
+type AvatarType = 'home' | 'header'
+
+function AvatarComponent({ type }: { type: AvatarType }) {
+  return (
+    <div
+      style="view-transition-name: avatar_transition"
+      class={[
+        `rounded-full flex p-[2px] transition-all duration-300`,
+        type === 'header' &&
+          `border border-solid border-gray-500/10 shadow-md dark:border-white/30 dark:border-dashed`,
+      ]}
+    >
+      <img src={Avatar} style={{ width: '100%', height: '100%', borderRadius: '100%' }} />
+    </div>
+  )
+}
+
+const handleAnimateAvatar = (avatar: HTMLElement | null, fromProps: gsap.TweenVars) => {
+  gsap.from(avatar, {
+    duration: 0.5,
+    ...fromProps,
+  })
+}
+
+const handleFromHome = (next: () => void) => {
+  renderHeaderAvatar()
+  document.startViewTransition(() => {
+    avatarFromHome?.remove()
+    headerAvatarContainer?.appendChild(avatarFromHeader!)
+    next()
+  })
+}
+
+const renderHeaderAvatar = () => {
+  avatarFromHeader = document.createElement('div')
+  render(<AvatarComponent type="header" />, avatarFromHeader!)
+}
+
+const handleBackHome = (refresh: boolean) => {
+  avatarFromHome = document.createElement('div')
+  render(<AvatarComponent type="home" />, avatarFromHome!)
+  if (refresh) {
+    homeAvatarContainer = document.getElementById('home_avatar_container')
+    homeAvatarContainer?.appendChild(avatarFromHome!)
+    handleAnimateAvatar(avatarFromHome, { opacity: 0, y: 50 })
+  } else {
+    initContainer(() => {
+      homeAvatarContainer = document.getElementById('home_avatar_container')
+      document.startViewTransition(() => {
+        avatarFromHeader?.remove()
+        homeAvatarContainer?.appendChild(avatarFromHome!)
+      })
+    })
+  }
+}
+
+const initContainer = (hook: () => void) => {
+  homeAvatarContainer = document.getElementById('home_avatar_container')
+  if (homeAvatarContainer) {
+    hook()
+  } else {
+    useMacroTask(() => {
+      initContainer(hook)
+    })
+  }
+}
+
+const useMacroTask = (fn: () => void) => {
+  setTimeout(fn, 0)
+}
+
+export default defineNuxtPlugin({
+  name: 'avatar-plugin',
+  async setup() {
+    const router = useRouter()
+    headerAvatarContainer = document.getElementById('header_avatar_container')
+
+    router.afterEach((to, from) => {
+      const toHomePage = to.path === '/'
+      toHomePage && handleBackHome(from.name === to.name)
+    })
+
+    router.beforeEach((to, from, next) => {
+      const refresh = from.name === to.name
+      if (refresh && to.path !== '/') {
+        renderHeaderAvatar()
+        headerAvatarContainer?.appendChild(avatarFromHeader!)
+        handleAnimateAvatar(avatarFromHeader, { opacity: 0, x: -50 })
+        next()
+      } else {
+        const fromHomePage = from.path === '/' && from.name !== to.name
+        fromHomePage ? handleFromHome(next) : next()
+      }
+    })
+  },
+})
