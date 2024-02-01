61f2c55e3dabbfaec7a21133f8badeb065c037e8
AceXiamo <xiamo@qwq.link>
2024-02-01 17:34:45 +0800

    feat: rename to `.tsx`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@unocss/nuxt', 'nuxt-icon', '@nuxtjs/color-mode'],
   css: ['~/assets/css/style.css'],
-  plugins: ['~/plugins/gsap.ts', '~/plugins/avatar.client.ts'],
+  plugins: ['~/plugins/gsap.ts', '~/plugins/avatar.client.tsx'],
   colorMode: {
     preference: 'system', // default value of $colorMode.preference
     fallback: 'light', // fallback value if not system preference found
