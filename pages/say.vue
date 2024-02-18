74e74ac908330c6ec5cff07ac9766b85c4ca8906
AceXiamo <xiamo@qwq.link>
2024-02-18 09:34:16 +0800

    feat: add gsap animate

--- pages/say.vue
+++ pages/say.vue
@@ -6,36 +6,37 @@
         class="w-[700px] rounded-md"
       />
     </div>
-    <div class="w-[700px] flex flex-col gap-[100px] mt-[50px]">
-      <!-- <template v-for="(item, index) in data.post_data">
-        <ChatItem :item="item" />
-      </template> -->
+    <div class="w-[700px] flex flex-col gap-[100px] mt-[100px]">
+      <template v-for="item in data?.post_data">
+        <ChatItem
+          :item="item"
+          v-gsap="{
+            method: 'from',
+            config: {
+              opacity: 0,
+              x: -50,
+              duration: .5,
+              ease: 'back(5)',
+              autoAlpha: 0,
+              scrollTrigger: true,
+            },
+          }"
+        />
+      </template>
     </div>
   </div>
 </template>
 
 <script lang="tsx" setup>
 import gsap from 'gsap'
+import Avatar from '~/assets/images/avatar.jpg'
 
 const cover = ref()
-const dot = ref()
 const page = ref(0)
 
-const handleFontAnime = () => {
-  gsap.to(dot.value, {
-    duration: 0.5,
-    repeat: -1,
-    yoyo: true,
-    opacity: 0,
-    yoyoEase: true,
-  })
-}
-
 onMounted(() => {
-  handleFontAnime()
-
   gsap.from(cover.value, {
-    duration: .5,
+    duration: 0.5,
     opacity: 0,
   })
 })
@@ -43,14 +44,15 @@ onMounted(() => {
 function ChatItem({ item }: { item: PostDataItem }) {
   return (
     <div class="flex gap-[20px]">
-      <img src="https://axm.moe/avatar" class="w-[40px] h-[40px] rounded-full" />
+      <img src={Avatar} class="w-[40px] h-[40px] rounded-full" />
       <div class="flex-auto flex flex-col">
         <div class="flex gap-[10px] items-center">
           <span class="text-gray-600 text-[12px]">xiamo</span>
           <span class="text-gray-400 text-[12px]">三天前</span>
         </div>
-        <div class="bg-black bg-opacity-[0] rounded-md mt-[10px]">
+        <div class="bg-black bg-opacity-[0] rounded-md mt-[10px] flex flex-col gap-[15px]">
           <div class="text-black dark:text-white text-[13px]">{item.content}</div>
+          {typeof item.attach !== 'string' && <ImageItem images={item.attach || []} />}
         </div>
         <div class="flex gap-[20px] text-gray-300 mt-[30px] justify-end items-center">
           <icon name="heroicons:heart" />
@@ -61,9 +63,30 @@ function ChatItem({ item }: { item: PostDataItem }) {
   )
 }
 
-// const { data, pending, refresh, error } = useFetch('/api/say', {
-//   params: {
-//     page,
-//   },
-// })
+function ImageItem({ images }: { images: PostAttachItem[] }) {
+  return (
+    <>
+      {images.length === 1 && (
+        <div class="w-[60%]">
+          <img class="object-cover w-full rounded-md" src={images[0].thum} />
+        </div>
+      )}
+      {images.length >= 2 && (
+        <div class="grid grid-cols-3">
+          {images.map(img => (
+            <div class="w-full">
+              <img class="max-h-[300px] object-cover w-full rounded-md" src={img.thum} />
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  )
+}
+
+const { data, pending, refresh, error } = useFetch('/api/say', {
+  params: {
+    page,
+  },
+})
 </script>
