bc081998aa0a94645b306af56d694ae6ccae9b72
AceXiamo <xiamo@qwq.link>
2024-02-19 10:30:08 +0800

    Add about page with basic information

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col items-center pt-[70px] pb-[200px]">
+    <div class="content-w text-black dark:text-white">
+      <h3>About Me</h3>
+      <div class="flex gap-[5px]">
+        <span class="text-[14px] text-gray-400">nick:</span>
+        <span class="text-[14px]">AceXiamo | xiamo</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Avatar from '~/assets/images/avatar.jpg'
+</script>
