51536f7f2f2b74d27adb78ce8fda4919bc5d0df9
AceXiamo <xiamo@qwq.link>
2024-02-20 14:38:12 +0800

    feat: modify menu & some css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="flex flex-col">
-    <div class="mt-[30px] w-full flex items-center justify-end relative px-[200px] box-border">
-      <div class="absolute left-[200px] w-[50px] h-[50px]" id="header_avatar_container"></div>
+    <div
+      class="mt-[30px] w-full flex items-center justify-end relative box-border lg:px-[200px] lt-lg:px-[20px]"
+    >
+      <div
+        class="absolute lg:left-[200px] lt-lg:left-[20px] lg:w-[50px] lg:h-[50px] lt-lg:w-[40px] lt-lg:h-[40px]"
+        id="header_avatar_container"
+      ></div>
       <MenuBar :active="active" />
       <div
-        class="ml-[50px] text-black dark:text-white w-[30px] h-[30px] rounded-[5px] flex transition-all duration-300 hover:bg-black/20 hover:dark:bg-white/20 cursor-pointer"
+        class="lg:ml-[50px] lt-lg:ml-[10px] text-black dark:text-white w-[30px] h-[30px] rounded-[5px] flex transition-all duration-300 hover:bg-black/20 hover:dark:bg-white/20 cursor-pointer"
         @click="toggleTheme"
       >
         <icon name="heroicons:light-bulb" class="m-auto" />
@@ -22,8 +27,18 @@
 <script lang="tsx" setup>
 const route = useRoute()
 const active = computed(() => route.name?.toString())
-let htmlElement: HTMLElement | null = null
 const colorMode = useColorMode()
+
+if (!import.meta.dev) {
+  useHead({
+    script: [
+      {
+        src: '//sdk.51.la/js-sdk-pro.min.js?id=KLxrr5PEkHmAt1nW&ck=KLxrr5PEkHmAt1nW',
+        id: 'LA_COLLECT',
+      },
+    ],
+  })
+}
 
 const to = (path: string) => {
   if (route.path === path) return
@@ -38,28 +53,33 @@ const to = (path: string) => {
 const toggleTheme = (e: MouseEvent) => {
   const theme = document.querySelector(':root') as HTMLElement
   theme.style.setProperty('view-transition-name', 'color_theme')
-  const transition = document.startViewTransition(() => {
+
+  if (document.startViewTransition) {
+    const transition = document.startViewTransition(() => {
+      colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+    })
+    transition.ready.then(() => {
+      theme.animate(
+        {
+          clipPath: [
+            `circle(0 at ${e.clientX}px ${e.clientY}px)`,
+            `circle(100% at ${e.clientX}px ${e.clientY}px)`,
+          ],
+        },
+        {
+          duration: 500,
+          easing: 'ease-in',
+          // 指定要附加动画的伪元素
+          pseudoElement: '::view-transition-new(color_theme)',
+        },
+      )
+    })
+    transition.finished.then(() => {
+      theme.style.removeProperty('view-transition-name')
+    })
+  } else {
     colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
-  })
-  transition.ready.then(() => {
-    theme.animate(
-      {
-        clipPath: [
-          `circle(0 at ${e.clientX}px ${e.clientY}px)`,
-          `circle(100% at ${e.clientX}px ${e.clientY}px)`,
-        ],
-      },
-      {
-        duration: 500,
-        easing: 'ease-in',
-        // 指定要附加动画的伪元素
-        pseudoElement: '::view-transition-new(color_theme)',
-      },
-    )
-  })
-  transition.finished.then(() => {
-    theme.style.removeProperty('view-transition-name')
-  })
+  }
 }
 
 const menu = [
@@ -82,12 +102,17 @@ const menu = [
     icon: 'heroicons:cube-transparent-16-solid',
   },
   {
-    title: 'Blog',
-    name: 'blog',
-    path: 'https://axm.moe',
-    icon: 'heroicons:puzzle-piece',
+    title: 'Photo',
+    name: 'photo',
+    path: '/photo',
+    icon: 'heroicons:photo-solid',
   },
-
+  // {
+  //   title: 'Blog',
+  //   name: 'blog',
+  //   path: 'https://axm.moe',
+  //   icon: 'heroicons:puzzle-piece',
+  // },
   // {
   //   title: 'Toy',
   //   name: 'toy',
@@ -104,10 +129,10 @@ const menu = [
 
 function MenuBar({ active }: { active?: string }) {
   return (
-    <div class="flex w-max p-[10px] rounded-md gap-[50px]">
+    <div class="flex w-max p-[10px] rounded-md lg:gap-[50px] lt-lg:gap-[20px]">
       {menu.map(item => (
         <div
-          class="flex items-center gap-[5px] text-[16px] cursor-pointer text-black dark:text-white relative group"
+          class="flex items-center gap-[5px] lg:text-[16px] lt-lg:text-[15px] cursor-pointer text-black dark:text-white relative group"
           onClick={() => to(item.path)}
         >
           <icon name={item.icon} />
