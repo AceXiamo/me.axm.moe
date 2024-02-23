ea5103181669848db4fda05c60a63bd5f8793b21
AceXiamo <xiamo@qwq.link>
2024-02-23 14:26:44 +0800

    Refactor photo.vue and photo.ts

--- pages/photo.vue
+++ pages/photo.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="flex flex-col items-center pt-[70px] pb-[200px]">
-    <div
-      class="lg:content-w lt-lg:w-[calc(100%-40px)] lg:mx-auto gap-[5px] relative"
-      id="image-container"
-    ></div>
+    <div class="lg:content-w lt-lg:w-[calc(100%-40px)] lg:mx-auto gap-[5px] relative" id="image-container"></div>
   </div>
 </template>
 
@@ -11,6 +8,9 @@
 import { render } from 'vue'
 import gsap from 'gsap'
 import { images } from '@/utils/photo'
+import imageViewer from '~/utils/imageViewer'
+import BuncingBall from '~/assets/images/buncing-ball.svg'
+import { sleep } from '~/utils'
 
 let imageContainer: null | HTMLElement = null,
   widthOfImage = 0,
@@ -23,8 +23,8 @@ let imageContainer: null | HTMLElement = null,
 onMounted(() => {
   rowNum = window.innerWidth < 992 ? 2 : 3
 
-  mdImages = images.value.map(url => {
-    const arr = url.split('.')
+  mdImages = images.value.map(item => {
+    const arr = item.url.split('.')
     const suffix = arr.pop()
     const name = arr.join('.')
     return `${name}.md.${suffix}`
@@ -81,15 +81,57 @@ const initImage = () => {
       lineArr[withRowNum] = (lineArr[withRowNum] || 0) + height + gap
       imageContainer!.style.height = `${Math.max(...lineArr)}px`
     }
-    render(<ImageNode src={url} />, div)
+    render(<ImageNode src={url} index={index} />, div)
   })
   imageContainer!.append(...imageNodes)
 }
 
-function ImageNode({ src }: { src: string }) {
+function ImageNode({ src, index }: { src: string; index: number }) {
   return (
-    <div class="flex">
-      <img src={src} class="w-full h-full rounded-sm" />
+    <div class="flex relative">
+      <img src={src} class="w-full h-full rounded-sm" onClick={(e: Event) => handleView(index, e)} />
+    </div>
+  )
+}
+
+const handleView = (index: number, e: Event) => {
+  if (images.value[index].full_loaded) {
+    imageViewer.show(images.value[index].url, e)
+  } else {
+    const el = (e.currentTarget || e.target) as HTMLElement
+    const parent = el.parentElement as HTMLElement
+    const div = document.createElement('div')
+    div.style.position = 'absolute'
+    div.style.inset = '0'
+    parent.append(div)
+    render(<ImageLoading />, div)
+    gsap.from(div, {
+      opacity: 0,
+      y: 20,
+      duration: 0.2,
+    })
+    const img = new Image()
+    img.src = images.value[index].url
+    Promise.all([sleep(1000), img.onload]).then(() => {
+      images.value[index].full_loaded = true
+      gsap.to(div, {
+        opacity: 0,
+        y: 20,
+        duration: 0.1,
+        onComplete: () => {
+          imageViewer.show(images.value[index].url, e)
+          div.remove()
+        },
+      })
+    })
+  }
+}
+
+function ImageLoading() {
+  return (
+    <div class="flex flex-col bg-black/20 absolute inset-0 items-center justify-center">
+      <img class="w-[40px] h-[40px] object-cover" src={BuncingBall} />
+      <span class="text-[12px] mt-[10px]">加载原图中</span>
     </div>
   )
 }
--- utils/photo.ts
+++ utils/photo.ts
@@ -1,28 +1,33 @@
-export const images = ref<string[]>([
-  'https://image.qwq.link/images/2023/01/01/mmexport1653278805867.jpg',
-  'https://image.qwq.link/images/2023/08/02/884519747.png',
-  'https://image.qwq.link/images/2023/12/31/Lie11u_20231231_163855040d4d5ccf1d02c5170.jpg',
-  'https://image.qwq.link/images/2023/12/26/Lie11u_20230401_064913404.jpg',
-  'https://image.qwq.link/images/2023/12/26/Lie11u_20230910_171300132.jpg',
-  'https://image.qwq.link/images/2023/12/26/Lie11u_20230910_161727020.jpg',
-  'https://image.qwq.link/images/2023/08/09/Lie11u_20230625_062553950.jpg',
-  'https://image.qwq.link/images/2023/12/31/Lie11u_20231231_1638229722ac071b78ae5d69f.jpg',
-  'https://image.qwq.link/images/2023/12/31/Lie11u_20231231_1640212914a2460627d18af29.jpg',
-  'https://image.qwq.link/images/2024/02/20/Lie11u_20231231_173905108.fix.jpg',
-  'https://image.qwq.link/images/2023/12/31/Lie11u_20231231_16381242969d58cdc59bd50e3.jpg',
-  'https://image.qwq.link/images/2023/12/26/Lie11u_20230910_173442871.jpg',
-  'https://image.qwq.link/images/2023/01/01/LiecaFinal_20220725_054322819.jpg',
-  'https://image.qwq.link/images/2023/01/01/IMG_20220522_193957.jpg',
-  'https://image.qwq.link/images/2023/01/01/IMG_20221112_190211.jpg',
-  'https://image.qwq.link/images/2023/01/01/IMG_20220723_183105.jpg',
-  'https://image.qwq.link/images/2023/01/01/LiecaFinal_20220725_052618567.jpg',
-  'https://image.qwq.link/images/2023/01/01/IMG_20220522_193155.jpg',
+export const images = ref<ImageItem[]>([
+  { url: 'https://image.qwq.link/images/2023/01/01/mmexport1653278805867.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2024/01/03/2301703584467_.pic_hd.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/12/31/Lie11u_20231231_163855040d4d5ccf1d02c5170.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/12/26/Lie11u_20230401_064913404.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/12/26/Lie11u_20230910_171300132.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/12/26/Lie11u_20230910_161727020.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/08/09/Lie11u_20230625_062553950.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/12/31/Lie11u_20231231_1638229722ac071b78ae5d69f.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/12/31/Lie11u_20231231_1640212914a2460627d18af29.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2024/02/20/Lie11u_20231231_173905108.fix.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/12/31/Lie11u_20231231_16381242969d58cdc59bd50e3.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/12/26/Lie11u_20230910_173442871.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/01/01/LiecaFinal_20220725_054322819.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/01/01/IMG_20220522_193957.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/01/01/IMG_20221112_190211.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/01/01/IMG_20220723_183105.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/01/01/LiecaFinal_20220725_052618567.jpg', full_loaded: false },
+  { url: 'https://image.qwq.link/images/2023/01/01/IMG_20220522_193155.jpg', full_loaded: false },
 ])
 
+export type ImageItem = {
+  url: string
+  full_loaded: boolean
+}
+
 export const loadLowerImgInSiteInit = () => {
-  images.value.forEach(url => {
+  images.value.forEach(item => {
     const img = new Image()
-    const arr = url.split('.')
+    const arr = item.url.split('.')
     const suffix = arr.pop()
     const name = arr.join('.')
     img.src = `${name}.md.${suffix}`
